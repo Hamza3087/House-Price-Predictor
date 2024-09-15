802532e13e9d163535eceae6639aa2ae50a8d54e
Hamza Tariq <128885243+Hamza3087@users.noreply.github.com>
2024-09-15 17:42:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import pickle
+"""
+Module for training a linear regression model on house price data.
+"""
 import os
+import pickle  # Standard library imports
+import pandas as pd  # Third-party imports
+from sklearn.linear_model import LinearRegression
 
 MODEL_PATH = 'model.pkl'
 CSV_FILE_PATH = 'house_data.csv'
 
+
 def load_data_from_csv(file_path):
-    # Load dataset from CSV
+    """
+    Load dataset from a CSV file.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        tuple: A tuple containing the house sizes and prices.
+    """
     data = pd.read_csv(file_path)
     house_sizes = data[['Size', 'Bedrooms', 'Bathrooms']].values
     house_prices = data['Price'].values
     return house_sizes, house_prices
 
+
 def train_model():
-    # Load data from CSV
+    """
+    Train a linear regression model on house data and save the model.
+
+    Loads house size and price data from a CSV file, trains a linear regression
+    model, and saves the trained model to a file.
+    """
     house_sizes, house_prices = load_data_from_csv(CSV_FILE_PATH)
-    
-    # Train the Linear Regression model
+
     model = LinearRegression()
     model.fit(house_sizes, house_prices)
-    
-    # Save the model to a file
+
     with open(MODEL_PATH, 'wb') as file:
         pickle.dump(model, file)
-    
+
     print("Model training complete. Model saved to 'model.pkl'.")
 
+
 if __name__ == "__main__":
-    # Train the model when the script is run
     print("Starting model training...")
     train_model()
     print("Model training finished.")
