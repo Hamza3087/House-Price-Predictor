5df321152828a012a2d96db08f15734e5d1836e2
Hamza Tariq <128885243+Hamza3087@users.noreply.github.com>
2024-09-15 17:57:26 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,39 @@
-import pandas as pd
-import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score
-import pickle
+"""
+Module for evaluating the linear regression model.
+"""
+import pickle  # Standard library imports
 import os
+import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
 
 MODEL_PATH = 'model.pkl'
 TEST_CSV_FILE_PATH = 'house_test_data.csv'
 
 def load_data_from_csv(file_path):
-    # Load dataset from CSV
+    """
+    Load dataset from CSV.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        tuple: A tuple containing the house sizes and prices.
+    """
     data = pd.read_csv(file_path)
     house_sizes = data[['Size', 'Bedrooms', 'Bathrooms']].values
     house_prices = data['Price'].values
     return house_sizes, house_prices
 
 def evaluate_model():
-    # Check if the model exists
+    """
+    Evaluate the model's performance on test data.
+
+    Raises:
+        FileNotFoundError: If the model file does not exist.
+    """
     if not os.path.exists(MODEL_PATH):
         raise FileNotFoundError("Model not found! Please train the model by running 'main.py' first.")
     
-    # Load model and test
     with open(MODEL_PATH, 'rb') as file:
         model = pickle.load(file)
 
